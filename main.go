0aa8a59c7682fa5b54f232d4da63176e6ca7b2cc
Puhkusarvikuono <tossu18@proton.me>
2026-02-06 13:47:25 +0200

    Repl function and commands

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
-import (
-	"fmt"
-	"bufio"
-	"os"
-	"github.com/Puhkusarvikuono/golangpokedex/cleaner"
-) 
 
-func main () {
-	scanner := bufio.NewScanner(os.Stdin)
-	for ;; {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading input:", err)
-		}
-		text := scanner.Text()
-		if text == "" { 
-			fmt.Println("Please input a command.")
-			continue
-		}
-		cleanText := cleaner.CleanInput(text)
-		fmt.Printf("Your command was: %s\n", cleanText[0])
-	}
+func main() {
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	// cleanInput splits text into lowercase words, trimming whitespace.
+	// Note: this could also be implemented with strings.Fields after
+	// trimming + lowercasing, but this version is expanded for clarity.
+
+	splitText := strings.Split(text, " ")
+	words := []string{}
+	for _, s := range splitText {
+		s = strings.TrimSpace(s)
+		s = strings.ToLower(s)
+		if s != "" {
+		words = append(words, s)
+		}
+	}
+	return words
+}
+
+type cliCommand struct {
+	name				string
+	description	string
+	callback		func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := map[string]cliCommand{
+		"exit": {
+			name: 				"exit",
+			description:	"Exit the Pokedex",
+			callback: 		commandExit,
+		},
+		"help": {
+			name:					"help",
+			description:	"Displays a help message",
+			callback:			commandHelp,	
+		},
+	}
+	for ;; {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		rawInput := scanner.Text()
+		if rawInput == "" {
+			fmt.Println("Please input a command. Type 'help' for help message.")
+			continue
+		}
+		input := cleanInput(rawInput)
+		if commandFunc, exists := commands[input[0]]; exists {
+			commandFunc.callback()
+		} else {
+			fmt.Println("Please input a command. Unknown command:", input[0])
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading input:", err)
+		}
+	}
+}
+
+
